57c2296743a2bdc3a00715394389510f57bb869d
mdulce <matiaodulce@gmail.com>
2019-08-26 20:15:59 -0300

    Notas

--- src/Paquete1/Gato.java
+++ src/Paquete1/Gato.java
@@ -36,6 +36,6 @@ public class Gato {
     public void  correrJerry (Raton jerry){ //El consumo de energía es 0.5 *  su velocidad * distancia entre ambos.
         // Además su posición actual pasa a ser igual a la del ratón.
         this.energia= getEnergia()- (0.5*getVelocidad()*Math.abs(jerry.getPosicion()- getPosicion()));//si esto lo pongo en otro metodo mejor
-        this.posicion=jerry.getPosicion();//se denomina abstraccion auxiliar
+        this.posicion=jerry.getPosicion();//se denomina abstraccion auxiliar - BUENA PRACTICA
     }
 }
